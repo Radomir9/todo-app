1eab57a97f4808649db3b93e54f28dc308ffbc9f
Radomir7 <radomirmilenkovic91@gmail.com>
2025-04-14 19:18:47 +0200

    Add basic layout and structure of todo app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,64 @@
 <template>
-  <div>
-    
-  </div>
+  <main>
+
+    <section>
+      <h2>
+        What's up, <input type="text" id="name" placeholder="Name here">
+      </h2>
+    </section>
+
+    <section>
+      <h3>CREATE A TODO</h3>
+
+      <form>
+        <h4>What's on your todo list?</h4>
+        <input type="text" name="content" id="content" placeholder="e.g. make a video" />
+
+        <h4>Pick a category</h4>
+        <div>
+
+          <label>
+            <input type="radio" name="category" id="category1" value="business" />
+            <!-- <span></span> -->
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input type="radio" name="category" id="category2" value="personal" />
+            <!-- <span></span> -->
+            <div>Personal</div>
+          </label>
+
+        </div>
+
+        <input type="submit" />
+      </form>
+    </section>
+
+    <section>
+      <h3>TODO LIST</h3>
+      <div>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+          </label>
+
+          <div>
+            <input type="text" />
+          </div>
+
+          <div>
+            <button>Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+  </main>
 </template>
+
 
 <script>
 export default {
@@ -10,5 +66,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
